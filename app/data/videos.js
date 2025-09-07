da0f351f8062918dc540c183f1761950514a5113
Sasini Tennakoon <tennakoonsasi5@gmail.com>
2025-09-07 14:00:05 +0530

    videography page

---
+++ app/data/videos.js
@@ -0,0 +1,107 @@
+// src/data/videos.js
+export const videos = [
+  {
+    title: 'Television Commercial for “MONARA” Newspaper',
+    tag: ["DOP" ],
+    description:
+      'A creative production by Xposure (Pvt) Ltd, this television commercial showcases the identity and vision of Monara Newspaper. Directed by Shashanka Vidanagama and cinematographed by Dinuka Gunawardana, the project blends impactful storytelling with compelling visuals.',
+    videoSrc: '/video/Monara TVC 1.Jpg',
+    youtubeUrl: 'https://www.facebook.com/share/v/19TZpHZsd8/?mibextid=wwXIfr'
+  },
+  {
+    title:
+      '"මාර්තු 31" | මිරිහානෙන් ඇරඹුණු ජනතා නැගිටීමක වසරක මතකාවලෝකනය | Documentary video',
+    tag: ['Cinematographer'],
+    description:
+      'This documentary reflects on the first anniversary of the historic public uprising that began in Mirihana on March 31. A project by Xposure Pvt Ltd, it is directed by Samith Bodhipaksha and filmed by Dinuka Gunawardana & Kasun Fernando, capturing powerful moments and personal accounts that shaped a pivotal movement in Sri Lanka’s recent history.',
+    videoSrc: '/video/Documentry.png',
+    youtubeUrl: 'https://youtu.be/35m-2nEdGs8?si=KYSnrl7-SiFUDYHa'
+  },
+  {
+    title: 'Television Commercial for "MONARA" Newspaper',
+    tag: ['DOP'],
+    description:
+      'A creative production by Xposure (Pvt) Ltd, this television commercial showcases the identity and vision of Monara Newspaper. Directed by Shashinka Vidusara and cinematographed by Dinuka Gunawardana, the project blends impactful storytelling with compelling visuals.',
+    videoSrc: '/video/monara tvc 2.jpg',
+    youtubeUrl: 'https://www.facebook.com/share/r/16wXZvCWGH/?mibextid=wwXIfr'
+  },
+  {
+    title: 'COVID 19 ආර්ථික අර්බුදය | Law & Societry Trust',
+    tag: ['DOP','Editor','colorist'],
+    description:
+      'A creative awareness video by Law & Society Trust, this production is part of the Pandemic Justice project. Directed by Nisanka Weheraduwa and cinematographed and edited by Dinuka Gunawardana, the video sheds light on the socio-economic impact of COVID-19 and the ongoing economic crisis through a visually compelling narrative.',
+    videoSrc: '/video/LST.jpg',
+    youtubeUrl: 'https://www.facebook.com/share/v/19P2RB73cp/?mibextid=wwXIfr',
+     previewImages: [
+    '/video/LST/Chess 1.jpg',
+    '/video/LST/Chess 2.jpg',
+    '/video/LST/Chess 3.jpg',
+    '/video/LST/Chess 4.jpg',
+  ]
+  },
+  {
+    title: 'Unclaimed Homeland (Resilience Amidst the Ruins of Mullikulam)',
+    tag: ['Editor','Colourist'],
+    description:
+      'Presented by PARL (The People’s Alliance for Right to Land) in collaboration with the Law & Society Trust, this video highlights the long-standing land rights struggle of the Tamil Catholic fishing community of Mullikulam in South Mannar. Produced by Amila Udagedara and edited by Dinuka Gunawardana, it documents the forced displacement and military occupation of over 8,000 acres of village land, once rich with agriculture, tanks, and community life.',
+    videoSrc: '/video/Documentry 2.jpg',
+    youtubeUrl: 'https://youtu.be/3zOScECU_3k?si=pZ6pa2sfksaN_w6c',
+    previewImages: [
+    '/video/doc2/1.png',
+    '/video/doc2/2.png',
+    '/video/doc2/3.png',
+    '/video/doc2/4.png',
+  ]
+  },
+  {
+    title: 'Gayora Cinnamon Tea Promo Video',
+    tag: ['Editor'],
+    description:
+      'Produced by Xposure (Pvt) Ltd for Gayora Tea, this commercial video captures the heritage and craftsmanship of Sri Lankan tea. Directed by Samith Bodhipaksha and edited by Dinuka Gunawardana, the film blends striking visuals with rich storytelling to portray the journey from leaf to cup.',
+    videoSrc: '/video/Tea short video.jpg',
+    youtubeUrl: 'https://youtu.be/LREbY8Ak5c8',
+    previewImages: [
+    '/video/Gayora/Gayora 1.jpg',
+    '/video/Gayora/Gayora 2.jpg',
+    '/video/Gayora/Gayora 3.jpg',
+    '/video/Gayora/Gayora 4.jpg',
+  ]
+  },
+  {
+    title: 'Premium Shoe Product Introduction',
+    tag: ['Videographer','Editor'],
+    description:
+      'Produced by Xposure Pvt Ltd and edited by Dinuka Gunawardana, this commercial video showcases an in-depth review of a premium shoe product, capturing its design, comfort, and performance features. Combining dynamic visuals and expert camera work by Dinuka Gunawardana, the video highlights the product’s aesthetic appeal and functionality, offering viewers a comprehensive look at what sets this footwear apart in today’s competitive market.',
+    videoSrc: '/video/Product video.jpg',
+    youtubeUrl: 'https://youtu.be/IFU9ifD5Wk8',
+    previewImages: [
+    '/video/Shoes/1.jpg',
+    '/video/Shoes/2.jpg',
+    '/video/Shoes/3.jpg',
+    '/video/Shoes/4.jpg',
+  ]
+  },
+  {
+    title: 'ජූලි 09ට පසුවදනක් Documentary Video',
+    tag: ['DOP','Editor','Colourist'],
+    description:
+      'Presented by Xposure Pvt Ltd, this documentary directed by Samith Bodhipaksha captures the spirit and resilience of the Sri Lanka Aragalaya movement. With compelling visuals and an evocative narrative, the film reflects a pivotal chapter in the nation’s contemporary history. Cinematography and editing by Dinuka Gunawardana.',
+    videoSrc: '/video/July 9.jpg',
+    youtubeUrl: 'https://youtu.be/_CSHO1bekqE?si=5n-yNP35P3EFuLEe',
+    previewImages: [
+    '/video/July 9/1.png',
+    '/video/July 9/2.png',
+    '/video/July 9/3.png',
+    '/video/July 9/4.png',
+  ]
+  },
+  {
+    title: 'පාර්ලිමේන්තු මැතිවරණය 2024 | සෑම් චන්ද්‍රසෝම | සන්දේශය',
+    tag: ['DOP','Colourist'],
+    description:
+      'Produced for the Sandeshaya Media Network YouTube channel, this Parliament Election Interview is directed by Kasun Fernando. The video offers timely insights into the political landscape through candid conversations and expert perspectives. Videography by Dinuka Gunawardana.',
+    videoSrc: '/video/6.jpg',
+    youtubeUrl: 'https://youtu.be/Ndsk4ZhuXdg',
+    
+  },
+];
