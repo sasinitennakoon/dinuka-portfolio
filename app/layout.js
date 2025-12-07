30015faf36da6375a4fd59bec9e4c89a131c0c6a
sasinitennakoon <123575090+sasinitennakoon@users.noreply.github.com>
2025-12-07 15:20:21 +0530

    Change language attribute from 'si' to 'en'

--- app/layout.js
+++ app/layout.js
@@ -89,7 +89,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="si"
+      lang="en"
       className={`${notoSinhala.variable} ${geistSans.variable} ${geistMono.variable} font-loading`}
     >
       <head>
