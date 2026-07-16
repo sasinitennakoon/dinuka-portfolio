bed72d08de2f5200895229deb82c8b0210c04c7a
Sasini Tennakoon <tennakoonsasi5@gmail.com>
2026-07-16 20:21:12 +0530

    add

--- app/design/page.js
+++ app/design/page.js
@@ -103,8 +103,8 @@ export default function GraphicDesignPage() {
         >
           <div className="columns-1 sm:columns-2 md:columns-3 gap-4 space-y-4">
             {[
-              "1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg",
-              "7.jpg","8.jpg","9.jpg","10.jpg","12.jpg","13.jpg","14.png","15.jpg",
+              "21.jpg","20.jpg","19.jpg","22.jpg","23.jpg","24.jpg","1.jpg","2.jpg","3.jpg","5.jpg","6.jpg",
+              "7.jpg","8.jpg","9.jpg","10.jpg","12.jpg","13.jpg","14.png","15.jpg","25.jpg",
             ].map((img, i) => (
               <Image
                 key={i}
